1f63cf491666cde8f6eb00deac84fba95d24e03e
PTimi97 <44099875+PTimi97@users.noreply.github.com>
2019-03-26 22:08:52 +0100

    Literally everything

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Uartos' 
+ * Target:  'Uartos' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
